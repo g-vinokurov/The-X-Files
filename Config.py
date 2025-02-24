f0c4b727ba78c8b1baf28c3fe5a2163341f05949
g-vinokurov <g.vinokurov@g.nsu.ru>
2025-02-24 15:35:56 +0700

    Refactoring

--- Config.py
+++ Config.py
@@ -1,12 +1,16 @@
 
+import dotenv
 import os
 
+env = dotenv.dotenv_values('.env')
+
+# Project config
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
+# Assets config
 FONTS_DIR = os.path.join(PROJECT_DIR, 'Gui', 'Fonts')
-ICONS_DIR = os.path.join(PROJECT_DIR, 'Gui', 'Icons')
-IMAGES_DIR = os.path.join(PROJECT_DIR, 'Gui', 'Images')
 
-LOG_LVL = 'CRITICAL'
-LOG_FILE = None
-LOG_FMT = '%(asctime)s %(levelname)s %(message)s'
+# Logging config
+LOG_LVL = env.get('LOG_LVL', 'CRITICAL')
+LOG_FILE = env.get('LOG_FILE', None)
+LOG_FMT = env.get('LOG_FMT', '%(asctime)s %(levelname)s %(message)s')
